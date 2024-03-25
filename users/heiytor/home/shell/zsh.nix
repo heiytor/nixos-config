cd39d170a71f83ad6ec91232179f5de2379c3786
Heitor Danilo <heitor.danilo@icloud.com>
2024-03-25 19:28:52 -0300

    direnv

--- users/heiytor/home/shell/zsh.nix
+++ users/heiytor/home/shell/zsh.nix
@@ -12,6 +12,13 @@
     enableAliases = true;
   };
 
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
+      enable = true;
+    };
+  };
+
   programs.zsh = {
     enable = true;
     dotDir = ".config/zsh";
