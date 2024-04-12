114e9364d3bbc1aca3860ff63cebeae0dcd0e4e9
Heitor Danilo <heitor.danilo@icloud.com>
2024-04-12 18:06:03 -0300

    fix default kitty font

--- users/heiytor/home/terminal/kitty.nix
+++ users/heiytor/home/terminal/kitty.nix
@@ -2,6 +2,13 @@
 
 {
   programs.kitty = {
+    package = pkgs.kitty;
     enable = true;
+    font = {
+      name = "JetBrainsMono";
+      size = 12;
+      package = pkgs.jetbrains-mono;
+    };
+    # theme = "";
   };
 }
